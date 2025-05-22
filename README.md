f2c8030de40b8ea35b438979b9af684248c421d0
Riccardo Grandi <riccardo.grandi@hyland.com>
2025-05-22 11:39:37 +0200

    Add metrics for sent message count

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # demo-scs-rabbit
+
+## Run RabbitMQ
+```bash
+docker run --name rabbitmq -p 5672:5672 -p 15672:15672 -e RABBITMQ_DEFAULT_USER=aaa -e RABBITMQ_DEFAULT_PASS=123456 rabbitmq:3.8-management
+```
+## Metrics
+Message sent count is available at the following endpoints
+```bash
+http://localhost:8080/actuator/prometheus
+http://localhost:8080/actuator/metrics/messages.sent.direct
+```
--- src/main/java/com/rgmf/demoscsrabbit/DemoScsRabbitApplication.java
+++ src/main/java/com/rgmf/demoscsrabbit/DemoScsRabbitApplication.java
@@ -1,11 +1,12 @@
 package com.rgmf.demoscsrabbit;
 
-import java.util.Date;
+import io.micrometer.core.instrument.MeterRegistry;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +17,10 @@ public class DemoScsRabbitApplication {
 
   private static final Logger log = LoggerFactory.getLogger(DemoScsRabbitApplication.class);
 
-  private AtomicInteger counter = new AtomicInteger();
+  @Autowired
+  private MeterRegistry meterRegistry;
+
+  private final AtomicInteger counter = new AtomicInteger();
 
   public static void main(String[] args) {
     SpringApplication.run(DemoScsRabbitApplication.class, args);
@@ -25,20 +29,23 @@ public class DemoScsRabbitApplication {
   @Bean
   public Supplier<String> source() {
     return () -> {
-      String message = "Ciao" + counter.incrementAndGet();
-      log.info("Sending value: " + message);
+      String message = "Message %s".formatted(counter.incrementAndGet());
+      log.info("Sending -> {}", message);
+
+      meterRegistry.counter("messages.sent.direct").increment();
+
       return message;
     };
   }
 
   @Bean
   public Consumer<String> sink() {
-    return message -> log.info("Received message " + message);
+    return message -> log.info("Received -> {}", message);
   }
 
   @Bean
   public Consumer<ErrorMessage> myErrorHandler() {
-    return v -> log.error("************ App in error {} **************", v.getOriginalMessage());
+    return error -> log.error("************ App in error {} ************", error.getOriginalMessage());
   }
 
 }
